b3f2b01cec4f85c6961aba6011368eac544fb6f5
Jesper Rønn-Jensen <jesperrr@gmail.com>
2022-04-18 22:48:37 +0200

    print HOME env variable on root page

--- src/main/java/dk/jesperronn/springboot26/controller/RootController.java
+++ src/main/java/dk/jesperronn/springboot26/controller/RootController.java
@@ -1,5 +1,7 @@
 package dk.jesperronn.springboot26.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,9 +10,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @ResponseBody
 public class RootController {
 
+  @Autowired
+  private Environment env;
+
   @GetMapping("/")
   public String index() {
-    return "Greetings from Spring Boot!";
+    return String.format("Greetings from Spring Boot, home: %s", env.getProperty("HOME"));
   }
 
 }
